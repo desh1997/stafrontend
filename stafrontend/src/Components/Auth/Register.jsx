7ea3c84bb2131e73f0dc9fd260ce0cf0788e3043
Deshbandhu <kingdeshbandhu@gmail.com>
2023-10-14 23:35:40 +0530

    fixed the registration css

--- stafrontend/src/Components/Auth/Register.jsx
+++ stafrontend/src/Components/Auth/Register.jsx
@@ -1,13 +1,24 @@
 import Axios from '../../utility/api';
 import '../Assets/auth.css';
+import logo from '../Assets/images/logo.jpg'
 
 export const Register = () => {
-
     return (
-        <div className='frame p-1'>
-            <div className='myDiv'>
-                <div className='main_content'>
-                    asdsadsdsadsa
+        <div className='reg_container'>
+            <div className='frame p-2'>
+                <div className='myDiv'>
+                    <div className='main_content'>
+                        <div className='auth_header'>
+                            <img className='logo' src={logo} alt="Your Logo"/>
+                        </div>
+                        asdasdasdasd<br/>
+                        asdasdasdasdasddsfgdfgsdfgsdfg<br/>
+                        asdasdasdasd<br/>
+                        asdasdasdasd<br/>
+                        asdasdasdasdasd<br/>
+                        asdasdasdasdasd<br/>
+                        asdasdasdasd<br/>
+                    </div>
                 </div>
             </div>
         </div>
