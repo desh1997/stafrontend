553980782050c4c82943d9ca00daf8d2fa60457b
Deshbandhu <kingdeshbandhu@gmail.com>
2023-10-28 21:56:48 +0530

    working on the response of nifty50

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,14 +4,20 @@ import requests
 
 from datetime import date
 from nsepython import index_history
+import json
+from django.http import JsonResponse
+import pandas as pd
 
 class Nifty50DataView(generics.GenericAPIView):
     def get(self, request):
-        # try:
-        symbol = "NIFTY 50"
-        start_date = "01-Mar-2011"
-        end_date = "07-Jun-2021"
-        print(index_history(symbol,start_date,end_date),"----dataaa---\n\n\n")
-        return Response({"ola":"uver"},status=status.HTTP_200_OK)
-        # except Exception as e:
-        #     return Response({"error": f"Failed to fetch Nifty50 data. Error: {str(e)}"}, status=500)
+        try:
+            symbol = "NIFTY 50"
+            start_date = "01-Mar-2011"
+            end_date = "07-Jun-2021"
+            df = index_history(symbol,start_date,end_date)
+            # df = pd.DataFrame(df)
+            # json_data = df.to_json(orient='records')
+            #json_data = df.to_dict()
+            return Response(df, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"error": f"Failed to fetch Nifty50 data. Error: {str(e)}"}, status=500)
